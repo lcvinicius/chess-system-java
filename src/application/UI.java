4a781f11df63432e51346746d6f69b770056313e
Lucas Vinícius <lucasvsn19@gmail.com>
2023-11-13 11:37:47 -0300

    Lógica de xequemate

--- src/application/UI.java
+++ src/application/UI.java
@@ -58,9 +58,16 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turno: " + chessMatch.getTurn());
-		System.out.println("Esperando o jogador: " + chessMatch.getCurrentPlayer());
-		if(chessMatch.getCheck()) {
-			System.out.println("CHECK!");
+
+		if (!chessMatch.getCheckMate()) {
+
+			System.out.println("Esperando o jogador: " + chessMatch.getCurrentPlayer());
+			if (chessMatch.getCheck()) {
+				System.out.println("CHECK!");
+			}
+		} else {
+			System.out.println("CHECKMATE !");
+			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
 		}
 	}
 
